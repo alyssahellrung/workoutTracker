3eaa08f67de631a7148e7603529d3ec872245c6d
alyssahellrung <alyssahellrung@gmail.com>
2020-07-01 19:43:42 -0700

    adjusting server for deployment

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(express.static("public"));
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 
+  "mongodb://user1:password1@ds035740.mlab.com:35740/heroku_tx7vd15q", 
+  { 
+    useNewUrlParser: true 
+  }
+  );
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
